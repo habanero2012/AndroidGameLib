fba029d7262b4b48e9a0c77a10517d159fe5e8f4
hiro <habanero2008@hotmail.co.jp>
2012-01-29 01:44:22 +0900

    add Random to Global.java

--- AndroidGameLib/src/jp/live/sato1101/androidgames/framework/Global.java
+++ AndroidGameLib/src/jp/live/sato1101/androidgames/framework/Global.java
@@ -1,8 +1,12 @@
 package jp.live.sato1101.androidgames.framework;
 
+import java.util.Random;
+
 public class Global {
 	public static int surfaceOffsetWidth;
 	public static int surfaceOffsetHeight;
 	public static int surfaceWidth;
 	public static int surfaceHeight;
+	
+	public static final Random rand = new Random(System.currentTimeMillis());
 }
